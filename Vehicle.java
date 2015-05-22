aa9f416d578f7ffe8ba82c43a58315bdafcd5574
Stephon Patton <stephonpatton@icloud.com>
2015-05-21 22:53:30 -0400

    Comments and abstract compareTo
    Documented methods and added compareTo method from Comparable interface

--- Vehicle.java
+++ Vehicle.java
@@ -4,45 +4,97 @@ public abstract class Vehicle implements Comparable {
 	private byte numberOfPassengers;
 	
 	public abstract int compareTo(Object obj);
+	/**
+	 * Default constructor. <br>
+	 * Sets the value by default to Ford Mustang 2003 5.
+	 */
 	public Vehicle() {
 		this("Ford", "Mustang", 2003, (byte)5);
 	}
+	/**
+	 * Overloading constructor that sets the make, model, <br>
+	 * and the number of passengers of a Vehicle object.
+	 * @param make
+	 * @param model
+	 * @param year
+	 * @param numOfPass
+	 */
 	public Vehicle(String make, String model, int year, byte numOfPass) {
 		this.setMake(make);
 		this.setModel(model);
 		this.setYear(year);
 		this.setNumberOfPassengers(numOfPass);
 	}
+	/**
+	 * Copy constructor
+	 * @param other
+	 */
 	public Vehicle(Vehicle other) {
 		other.setMake(this.make);
 		other.setModel(this.model);
 		other.setNumberOfPassengers(numberOfPassengers);
 		other.setYear(year);
 	}
+	/**
+	 * Gets the make from the Vehicle object
+	 * @return Make of the Vehicle object
+	 */
 	public String getMake() {
 		return this.make;
 	}
+	/**
+	 * Sets the make of the Vehicle object
+	 * @param make
+	 */
 	public void setMake(String make) {
 		this.make = make;
 	}
+	/**
+	 * Gets the model of the Vehicle object
+	 * @return Model of the Vehicle object
+	 */
 	public String getModel() {
 		return this.model;
 	}
+	/**
+	 * Sets the model of the Vehicle object
+	 * @param model
+	 */
 	public void setModel(String model) { 
 		this.model = model;
 	}
+	/**
+	 * Gets the year of the Vehicle object
+	 * @return Year of the Vehicle object
+	 */
 	public int getYear() {
 		return this.year;
 	}
+	/**
+	 * Sets the year of the Vehicle object
+	 * @param year
+	 */
 	public void setYear(int year) {
 		this.year = year;
 	}
+	/**
+	 * Gets the number of passengers of the Vehicle object
+	 * @return numberOfPassengers
+	 */
 	public byte getNumberOfPassengers() {
 		return this.numberOfPassengers;
 	}
+	/**
+	 * Sets numberOfPassengers of the Vehicle object
+	 * @param numberOfPassengers
+	 */
 	public void setNumberOfPassengers(byte numberOfPassengers) {
 		this.numberOfPassengers = numberOfPassengers;
 	}
+	/**
+	 * String representation of the Vehicle object. <br>
+	 * Prints out the year, make, mode, and numberOfPassengers of Vehicle object
+	 */
 	@Override
 	public String toString() {
 		return year + " " + make + " " + model + " seats " + numberOfPassengers + " people";
@@ -57,6 +109,10 @@ public abstract class Vehicle implements Comparable {
 		result = prime * result + year;
 		return result;
 	}
+	/**
+	 * Checks to see if two Vehicle objects are equal.
+	 * @return true or false
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -82,6 +138,11 @@ public abstract class Vehicle implements Comparable {
 			return false;
 		return true;
 	}
+	/**
+	 * Calculates the tax of a Vehicle object based on <br>
+	 * the type of Vehicle. 
+	 * @return tax
+	 */
 	public double calculateMichaelTax() {
 		double tax = ((.07*numberOfPassengers)/(Math.pow(1+2015-year, .84)))*100;
 		return tax;
